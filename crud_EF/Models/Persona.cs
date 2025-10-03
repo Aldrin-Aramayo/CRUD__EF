cd5fd8982ea0272474c7b8b7c77f29bfbc151c9a
charles <celta.loxcel@gmail.com>
2025-10-02 21:28:04 -0400

    actualizacion parcial

--- crud_EF/Models/Persona.cs
+++ crud_EF/Models/Persona.cs
@@ -2,12 +2,32 @@
 
 namespace crud_EF.Models
 {
-    internal class Persona
+    public class Persona
     {
         [Key]
         public int Id { get; set; }
         public string Nombres { get; set; }
         public int Edad { get; set; }
         public string Direccion { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Persona persona &&
+                   Id == persona.Id &&
+                   Nombres == persona.Nombres &&
+                   Edad == persona.Edad &&
+                   Direccion == persona.Direccion;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nombres, Edad, Direccion);
+        }
+
+        public override string ToString()
+        {
+            string texto = $"nombre: {this.Nombres} edad: {this.Edad} dire: {this.Direccion}";
+            return texto;
+        }
     }
 }
